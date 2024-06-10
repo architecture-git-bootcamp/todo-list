7fadf9b5ea56b3180b4cc535363d3970f92bdc96
lukvxx <lukas@deward.de>
2024-06-10 09:59:07 +0200

    add menu icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <v-layout>
     <v-app :theme="theme">
-      <v-navigation-drawer v-model="open">
+      <v-navigation-drawer v-model="open"        
+        :temporary="$vuetify.display.mobile"
+>
         <img class="ml-4" src="/public/logo.svg">
         <v-btn prepend-icon="mdi-plus" block variant="tonal" color="green">Neuer Task</v-btn>
       </v-navigation-drawer>
 
-      <v-app-bar class="px-3">
+      <v-app-bar class="px-3" >
+        <v-btn v-if="$vuetify.display.mobile" icon="mdi-menu" @click="open = !open"></v-btn>
         <h1>Übersicht</h1>
         <v-spacer></v-spacer>
 
@@ -19,6 +23,7 @@
       <v-main>
       </v-main>
     </v-app>
+    </v-layout>
 </template>
 
 <script setup lang="ts">
