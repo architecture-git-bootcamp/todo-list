494516fc4b6c3aae698eb37722c85e5ecb79c623
lukvxx <lukas@deward.de>
2024-06-12 09:23:35 +0200

    inital bad commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <v-layout>
     <v-app :theme="theme">
-      <v-navigation-drawer v-model="open"        
-        :temporary="$vuetify.display.mobile"
->         
+      <v-navigation-drawer v-model="open" :temporary="$vuetify.display.mobile">
         <img class="ml-4" src="/public/logo.svg">
-        <Menu />
+        <v-list-item class="d-flex justify-sm-start" @click="">Todo
+        </v-list-item>
+        <v-list-item class="d-flex justify-sm-start" @click="">Done
+        </v-list-item>
       </v-navigation-drawer>
 
       <v-app-bar class="px-2" elevation="0">
@@ -13,25 +14,102 @@
         <h1>Dashboard</h1>
         <v-spacer></v-spacer>
 
-        <v-btn
-          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          slim
-          @click="toggleTheme"
-        ></v-btn>
+        <v-btn :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" slim @click="toggleTheme"></v-btn>
       </v-app-bar>
 
       <v-main>
-        <todo-list />
+        <v-text-field v-model="newTodo" id="newTodo" name="newTodo" label="Aufgabe eingeben" @keyup.enter="addTodo"
+          persistent-hint />
+        <v-btn prepend-icon="mdi-plus" block variant="tonal" color="green" @click="addTodo">Hinzufügen</v-btn>
+        <v-list>
+          <v-list-item v-for="todo in todos" class="d-flex justify-sm-start">
+            <template #prepend>
+              <v-checkbox v-if="todo.status == 'open'" @click="markAsDone(todo.id)" hide-details></v-checkbox>
+
+              <v-btn v-else color="red" @click="deleteTodo(todo.id)" icon="mdi-delete" elevation="0" variant="text">
+              </v-btn>
+
+            </template>
+
+            <div>
+              <v-list-item-title>
+                <template v-if="todo.status == 'open'">
+                  {{ capitalize(todo.title) }}
+                </template>
+                <strike v-else>
+                  {{ capitalize(todo.title) }}
+                </strike>
+              </v-list-item-title>
+              <v-list-item-subtitle>Added on: {{ todo.createdAt }}</v-list-item-subtitle>
+            </div>
+            <div></div>
+          </v-list-item>
+        </v-list>
       </v-main>
     </v-app>
-    </v-layout>
+  </v-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import TodoList from './components/TodoList.vue';
-// @ts-ignore
-import Menu from './components/Menu.vue';
+import { computed, onMounted, ref } from 'vue'
+
+import { watch } from 'vue';
+
+import type { Todo } from './types/todo';
+
+
+function capitalize(value: string) {
+  if (!value) return "";
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+
+
+const newTodo = ref('')
+
+const todos = ref<Todo[]>([])
+
+watch(todos, () => console.log(todos.value), { deep: true });
+
+
+function addTodo() {
+  todos.value.push(
+    {
+      id: Math.random().toString(),
+      createdAt: new Date(),
+      status: 'open',
+      title: newTodo.value
+    }
+  )
+}
+
+function deleteTodo(todoId: string) {
+  todos.value = todos.value.filter(todo => todo.id !== todoId)
+}
+
+function markAsDone(todoId: string) {
+  todos.value = todos.value.map(todo => {
+    if (todo.id == todoId) {
+      return {
+        ...todo,
+        status: 'done'
+      } as Todo
+    }
+    return todo
+  })
+}
+
+onMounted(() => {
+  if (localStorage.getItem("todos")) {
+    todos.value = JSON.parse(localStorage.getItem("todos") ?? '') ?? [];
+  }
+})
+
+watch(todos, () => {
+  localStorage.setItem("todos", JSON.stringify(todos.value))
+}, { deep: true })
+
 
 type Theme = "light" | "dark";
 
@@ -39,7 +117,7 @@ const theme = ref<Theme>('light')
 
 const open = ref(true)
 
-function toggleTheme () {
+function toggleTheme() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
 </script>
--- src/types/todo.ts
+++
@@ -1,8 +0,0 @@
-export type Todo = {
-    id: string,
-    title: string,
-    createdAt: Date,
-    status: Status
-}
-
-export type Status = 'done' | 'open'
