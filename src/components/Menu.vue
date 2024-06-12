2acab0af7109f423fecca4aa91b9fe9c0dfe9e3b
BUCHENBERL <levi.buchenberg@dachser.com>
2024-06-12 10:23:16 +0200

    Added button functionality

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,11 +1,18 @@
 <template>
-    <v-list-item class="d-flex justify-sm-start"
-      @click="">Todo
+    <v-list-item class="d-flex justify-sm-start" :class="todoStore.selectedMenuEntry == 'all' ? 'menu-active' : ''"  @click="todoStore.selectedMenuEntry = 'all'">Alle
     </v-list-item>
-    <v-list-item class="d-flex justify-sm-start"
-      @click="">Done
+    <v-list-item class="d-flex justify-sm-start" :class="todoStore.selectedMenuEntry == 'open' ? 'menu-active' : ''" @click="todoStore.selectedMenuEntry = 'open'">Offen
     </v-list-item>
   </template>
 
-  <script setup>
-  </script>
+<script setup lang="ts">
+  import { useTodosStore } from '../stores/todos';
+
+  const todoStore= useTodosStore()
+</script>
+
+<style scoped>
+  .menu-active {
+    background-color: rgba(43, 49, 54, 0.374);
+  }
+</style>
